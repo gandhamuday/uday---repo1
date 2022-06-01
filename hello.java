718df53c0f311a38b497ffc845edded70a90be9d
lik <dev2@mail.com>
2022-06-01 16:51:02 +0530

    dev2

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
-hello every one 
+hello every one     #dev2
 hai all
-any one in class
+any one in class    #dev2
 class have static percent
 class topper was mani
 class cr was lik
-class have 10 mwmbers
+class have 10 mwmbers  #dev2
+
 
 
